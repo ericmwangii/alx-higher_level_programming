065d62c594d32781b9b99caf47b17b0d2a9e19be
ericmwangii <ericmwangii101@gmail.com>
2022-06-28 13:02:27 +0300

    print format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
     for i in range(len(temp)):
         if temp[i] >= 'a' and temp[i] <= 'z':
             temp[i] = chr(ord(temp[i]) - 32)
-    print("".join(temp))
+    print("{}".format("".join(temp)))
