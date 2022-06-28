227bd1ad6817228676198a0c04cdcd13778217d8
ericmwangii <ericmwangii101@gmail.com>
2022-06-28 12:31:48 +0300

    if, else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    temp = list(str)
+    for i in range(len(temp)):
+        if temp[i] >= 'a' and temp[i] <= 'z':
+            temp[i] = chr(ord(temp[i]) - 32)
+    print("".join(temp))
