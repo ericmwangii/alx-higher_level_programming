d558ca1d7800475ebcd9689503182f757f0cd566
ericmwangii <ericmwangii101@gmail.com>
2022-07-01 13:04:32 +0300

    task 0 - 7

---
+++ 0x03-python_data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=" ")
+        print("")
+    return matrix
