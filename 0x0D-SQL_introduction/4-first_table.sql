b7a85b6c51e6013624d15fb4ad539a3add536984
ericmwangii <ericmwangii101@gmail.com>
2022-09-06 18:00:35 +0300

    sql introduction

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Create a table called 'first_table'
+-- 'first_table' should have (id INT) and (name VARCHAR(256))
+-- If table 'first_table' already exists, should not fail.
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- List all rows of 'first_table'
+-- db name will be passed as arg to mysql cmd
+SELECT * FROM first_table;
